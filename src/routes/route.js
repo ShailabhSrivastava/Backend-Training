23ca4109dd2de55356211b887201165e520aef10
Shailabh <shaisri681@gmail.com>
2022-08-08 13:33:06 +0530

    Iam

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,27 @@
 const express = require('express');
-const abc = require('../logger/logger.js')
-const def = require('../util/helper.js')
-const ghi = require('../validator/formatter.js')
+const underscore = require('underscore')
+const _ = require('lodash')
+// const abc = require('../logger/logger.js')
+// const def = require('../util/helper.js')
+// const ghi = require('../validator/formatter.js')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.myWelcome()
-    def.printDate().printMonth().getBatchInfo()
-    ghi.LowerCase().UpperCase().Trim()
+    // console.log('My batch is', abc.name)
+    // abc.myWelcome()
+    // def.printDate().printMonth().getBatchInfo()
+    // ghi.LowerCase().UpperCase().Trim()
+    // const _ = require("lodash");
+    let arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    console.log(_.chunk(arr, 3));
+    let x = [1,3,5,7,9,11,13,15,17,19]
+    let newArray = _.tail(x);
+    console.log(newArray);
+    let newArr= _.union([1, 2, 3 ,3, 4, 5]);
+    console.log(newArr);
+    let pairs = [['horror','the shinig'], ['drama','titanic'], ['thriller','Shutter Islabd']] 
+    let obj = _.fromPairs(pairs);
+    console.log(obj)
     res.send('My second ever api!')
 });
 
