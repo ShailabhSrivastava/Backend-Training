057ea2a0549ef7c1acf9f979d7c46294858ce221
Shailabh <shaisri681@gmail.com>
2022-08-10 15:38:02 +0530

    Iam

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,52 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+    let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+                ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+                ]
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+                ]
+        },
+    ]
+    router.post('/players', function (req, res) {
+        let play = req.body;
+        let name1 = play.name;
+        let match = false;
+        for (let i = 0; i < players.length; i++) {
+            let name2 = players[i].name
+            if (name1  == name2) {
+                match = true;
+                break;
+            }
+        }
+        if (match == false) {
+            players.push(play);
+        }
+    res.send(  { data: players , status: true }  )
+    })
+
+
 module.exports = router;
