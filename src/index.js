74df6262708d012adec9d3ad292e8406cccc472a
Shailabh <shaisri681@gmail.com>
2022-08-13 09:46:00 +0530

    Iam

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://ShailabhSrivastava:LtR74yQBXKkSdvyd@cluster0.cxb6bki.mongodb.net/Shailabh-db", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
